1cb7ea16e28ef538ff4d4a1b0b24826519f56ec9
tanavsharma <sharma.tanav95@gmail.com>
2022-02-01 13:45:25 -0500

    Initial Commit

---
+++ AnimalSlotsGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimalSlotsGame
+//
+//  Created by Tanav Sharma on 2022-02-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
