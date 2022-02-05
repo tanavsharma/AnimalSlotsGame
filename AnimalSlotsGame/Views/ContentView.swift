f0c3de63f6c884d1909d9cb8dcb3a64ea5e61911
tanavsharma <sharma.tanav95@gmail.com>
2022-02-05 16:15:24 -0500

    shows custom popup if user wins the jackpot. Implemented random and probability of winning

--- AnimalSlotsGame/Views/ContentView.swift
+++ AnimalSlotsGame/Views/ContentView.swift
@@ -13,17 +13,20 @@ struct ContentView: View {
     
     //Create Array to Hold all the slot images
     
-    let images = ["sloth","sloth","monkey","lion","lion", "racoon","seven"]
+    let images = ["sloth","monkey","lion", "racoon","seven"]
+    
     
     // Creating variables
+    @State private var jackpot: Int = 25000
     @State private var showingAlert = false
     @State private var coins: Int = 2500
     @State private var betAmount: Int = 10
     @State private var amountOne: Int = 0
     @State private var showingModal: Bool = false
     @State private var showingWinAmount: Bool = false
+    @State private var showingJackpotAmount: Bool = false
     @State private var disabledBtn: Bool = false
-    
+    @State private var numberOfTries = 0
     
     /* Creating an array for each slot:
      * Index 0 = Slot On Top
@@ -45,16 +48,35 @@ struct ContentView: View {
     
     // MARK: Spin the slots
     func spinSlots(){
+        var rndImages = images.shuffled().shuffled().shuffled()
         slots = slots.map({ _ in
             Int.random(in: 0...images.count - 1)
         })
+        let hasAllItemsEqual = slots.dropFirst().allSatisfy({ $0 == slots.first})
+        
+        if hasAllItemsEqual == true && numberOfTries < 10{
+            rndImages = images.shuffled().shuffled()
+            slots = slots.map({ _ in
+                Int.random(in: 0...rndImages.count - 1)
+            })
+            
+        }
     }
     
     // Check To See if the player won
     func checkWinning(){
-        if slots[0] == slots[1] && slots[1] == slots[2] && slots[0] == slots[2] {
+        
+        let hasAllItemsEqual = slots.dropFirst().allSatisfy({$0 == slots.first})
+        if hasAllItemsEqual {
+            let index = slots[0]
+            if images[index] == "seven"{
+                //Jackpot Won
+                showingJackpotAmount = true
+            }
+            numberOfTries = 0
             self.playerWins()
         }else{
+            numberOfTries = numberOfTries + 1
             playerLoses()
         }
     }
@@ -71,6 +93,7 @@ struct ContentView: View {
         coins -= betAmount
     }
     
+    // MARK: Bet Checker
     func checkBetAmount(){
         if betAmount == 100{
             if coins < betAmount{
@@ -79,7 +102,7 @@ struct ContentView: View {
         }
     }
     
-    // Game Over Popup
+    //MARK: Game Over Popup
     func gameOver(){
         if coins <= 0{
             showingModal = true
@@ -97,14 +120,14 @@ struct ContentView: View {
                 LogoView()
                 Spacer()
                 
-                //JACKPOT
+                //MARK: JACKPOT LABELS
                 HStack{
                     Text("JACKPOT".uppercased())
                         .numberStyle()
 
                         .multilineTextAlignment(.trailing)
                     
-                    Text("$25,000")
+                    Text("\(jackpot)")
                         .numberStyle()
                         .layoutPriority(1)
                 }
@@ -118,10 +141,10 @@ struct ContentView: View {
                 
                 Spacer()
                 
-                //SLOTS
+                //MARK: SLOTS Handlers
                 VStack(alignment: .center, spacing: 0){
                     
-                    // Slot 1
+                    // MARK: Slot 1
                     ZStack{
                         SlotsView()
                         Image(images[slots[0]])
@@ -131,7 +154,7 @@ struct ContentView: View {
                     }
                     
                     HStack(alignment: .center, spacing: 0){
-                        // SLot 2
+                        // MARK: SLot 2
                         ZStack{
                             SlotsView()
                             Image(images[slots[1]])
@@ -141,7 +164,7 @@ struct ContentView: View {
                         }
                         
                         Spacer()
-                        // SLot 3
+                        // MARK: SLot 3
                         ZStack{
                             SlotsView()
                             Image(images[slots[2]])
@@ -152,7 +175,7 @@ struct ContentView: View {
                         
                     }
                     
-                    // Spin Button
+                    //MARK: Spin Button
                     Button(action: {
                         //Spin the slots
                         self.spinSlots()
@@ -168,12 +191,11 @@ struct ContentView: View {
                 }
                 .layoutPriority(2)
                 .disabled(coins <= 0)
-                
-                // FUNCTIONS
+            
                
-                // USER INFORMATION
+                //MARK: USER INFORMATION
                 HStack{
-                    //Your total Coins
+                    //MARK: Your total Coins Label
                     HStack{
                         Text("Your\nCoins".uppercased())
                             .labelStyle()
@@ -192,7 +214,7 @@ struct ContentView: View {
                     )
                     
                     Spacer()
-                    // Your Bet
+                    // MARK: Your Bet Label
                     HStack{
                         Text("\(betAmount)")
                             .numberStyle()
@@ -219,7 +241,7 @@ struct ContentView: View {
                 HStack{
                     
                     HStack{
-                        //Button - Bet 10
+                        //MARK: Button - Bet 10
                         Button(action: {
                             betAmount = 10
                         }){
@@ -245,7 +267,7 @@ struct ContentView: View {
                     Spacer()
                     
                     HStack{
-                        //Button - Bet 100
+                        //MARK: Button - Bet 100
                         Button(action: {
                             betAmount = 100
                         }){
@@ -356,6 +378,62 @@ struct ContentView: View {
                     //Message
                         VStack(alignment: .center, spacing: 16){
                             Text("You Won $\(amountOne)")
+                                .font(.system(.body,design: .rounded))
+                                .lineLimit(2)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Color.gray)
+                                .layoutPriority(1)
+                        }
+                        
+                        Spacer()
+                        
+                        //Button - Restart the game
+                        Button(action:{
+                            self.showingWinAmount = false
+                        }){
+                            Text("CONTINUE")
+                                .font(.system(.body, design: .rounded))
+                                .fontWeight(.semibold)
+                                .accentColor(Color("informationBg"))
+                                .padding(.horizontal, 12)
+                                .padding(.vertical, 8)
+                                .frame(minWidth: 128)
+                                .background(
+                                    Capsule()
+                                        .strokeBorder(lineWidth: 1.75)
+                                        .foregroundColor(Color("informationBg"))
+                                )
+                            
+                        }
+                    
+                        Spacer()
+                        
+                    }
+                    .frame(minWidth: 280, idealWidth: 280, maxWidth: 320, minHeight: 200, idealHeight: 220, maxHeight: 320, alignment: .center)
+                    .background(Color.white)
+                    .cornerRadius(20)
+                }
+            }
+            
+            //MARK: POP-UP for Jackpot Win
+            if $showingJackpotAmount.wrappedValue {
+                ZStack{
+                    Color(UIColor(red: 0, green: 0, blue: 0, alpha: 0.4)).edgesIgnoringSafeArea(.all)
+                    
+                    //Title - Game Over
+                    VStack(spacing: 0){
+                        Text("!!JACKPOT!!")
+                            .font(.system(.title,design: .rounded))
+                            .fontWeight(.heavy)
+                            .padding()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .background(Color("informationBg"))
+                            .foregroundColor(Color.white)
+                        Spacer()
+                        
+                    //Message
+                        VStack(alignment: .center, spacing: 16){
+                            Text("You Won The JACKPOT amount of\n $\(jackpot)")
                                 .font(.system(.body,design: .rounded))
                                 .lineLimit(2)
                                 .multilineTextAlignment(.center)
